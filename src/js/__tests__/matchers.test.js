8490e9f145f88da920a257352d8337a40cfcfbe6
Kate Sidyakina <therinbae@gmail.com>
2023-08-15 16:44:00 +0300

    matchers-homework done

---
+++ src/js/__tests__/matchers.test.js
@@ -0,0 +1,18 @@
+import matchIndicator from '../matchers';
+
+test('matchedIndicators test', () => {
+  const users = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+
+  const result = matchIndicator(users);
+  const expected = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+
+  expect(result).toEqual(expected);
+});
